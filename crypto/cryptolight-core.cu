af0071c91bd0fb88d2ab7250714b5eb4b57eda14
Noob-X <ajnatakaundinya@gmail.com>
2019-01-05 22:17:59 +0800

    Optimize Aeon cryptolight.

--- crypto/cryptolight-core.cu
+++ crypto/cryptolight-core.cu
@@ -25,17 +25,18 @@ __device__ __forceinline__ uint64_t cuda_mul128(uint64_t multiplier, uint64_t mu
 }
 
 __global__
-void cryptolight_core_gpu_phase1(int threads, uint32_t * long_state, uint32_t * ctx_state, uint32_t * ctx_key1)
+void cryptolight_core_gpu_phase1(const int threads, uint32_t * __restrict__ long_state,
+	const uint32_t * ctx_state, const uint32_t * ctx_key1)
 {
 	__shared__ uint32_t __align__(16) sharedMemory[1024];
-
-	cn_aes_gpu_init(sharedMemory);
 
 	const int thread = (blockDim.x * blockIdx.x + threadIdx.x) >> 3;
 	const int sub = (threadIdx.x & 7) << 2;
 
-	if(thread < threads)
+	if (thread < threads)
 	{
+		cn_aes_gpu_init(sharedMemory);
+
 		const int oft = thread * 50 + sub + 16; // not aligned 16!
 		const int long_oft = (thread << LONG_SHL_IDX) + sub;
 		uint32_t __align__(16) key[40];
@@ -50,7 +51,8 @@ void cryptolight_core_gpu_phase1(int threads, uint32_t * long_state, uint32_t * 
 		AS_UINT2(&text[2]) = AS_UINT2(&ctx_state[oft + 2]);
 
 		__syncthreads();
-		for(int i = 0; i < LONG_LOOPS32; i += 32) {
+
+		for (int i = 0; i < LONG_LOOPS32; i += 32) {
 			cn_aes_pseudo_round_mut(sharedMemory, text, key);
 			AS_UINT4(&long_state[long_oft + i]) = AS_UINT4(text);
 		}
@@ -211,12 +213,13 @@ void cryptolight_old_gpu_phase2(const int threads, const int bfactor, const int 
 #endif // __CUDA_ARCH__ >= 300
 }
 
-__device__ __forceinline__ void store_variant1(uint32_t* long_state)
+__device__ __forceinline__ void store_variant1(uint32_t * __restrict__ long_state, const uint4 ls)
 {
-	uint4* Z = (uint4*) long_state;
-	const uint32_t tmp = (Z->z >> 24); // __byte_perm(src, 0, 0x7773);
+	uint4 Z = ls;
+	const uint32_t tmp = (Z.z >> 24); // __byte_perm(src, 0, 0x7773);
 	const uint32_t index = (((tmp >> 3) & 6u) | (tmp & 1u)) << 1;
-	Z->z = (Z->z & 0x00ffffffu) | ((tmp ^ ((0x75310u >> index) & 0x30u)) << 24);
+	Z.z = (Z.z & 0x00ffffffu) | ((tmp ^ ((0x75310u >> index) & 0x30u)) << 24);
+	AS_UINT4(long_state) = Z;
 }
 
 #define MUL_SUM_XOR_DST_1(a,c,dst,tweak) { \
@@ -228,47 +231,51 @@ __device__ __forceinline__ void store_variant1(uint32_t* long_state)
         ((uint64_t *)dst)[1] = lo ^ tweak; }
 
 __global__
-void cryptolight_gpu_phase2(const uint32_t threads, const uint16_t bfactor, const uint32_t partidx,
+void cryptolight_gpu_phase2(const int threads, const uint16_t bfactor, const uint32_t partidx,
 	uint32_t * __restrict__ d_long_state, uint32_t * __restrict__ d_ctx_a, uint32_t * __restrict__ d_ctx_b,
 	uint64_t * __restrict__ d_tweak)
 {
 	__shared__ __align__(16) uint32_t sharedMemory[1024];
-	cn_aes_gpu_init(sharedMemory);
-	__syncthreads();
 
-	const uint32_t thread = blockDim.x * blockIdx.x + threadIdx.x;
+	const int thread = blockDim.x * blockIdx.x + threadIdx.x;
+
 	if (thread < threads)
 	{
+		cn_aes_gpu_init(sharedMemory);
+
 		const uint32_t batchsize = ITER >> (2 + bfactor);
 		const uint32_t start = partidx * batchsize;
 		const uint32_t end = start + batchsize;
 		const uint32_t longptr = thread << LONG_SHL_IDX;
 		uint32_t * long_state = &d_long_state[longptr];
-		uint64_t tweak = d_tweak[thread];
+		uint64_t tweak = __ldg(&d_tweak[thread]);
 
-		void * ctx_a = (void*)(&d_ctx_a[thread << 2]);
-		void * ctx_b = (void*)(&d_ctx_b[thread << 2]);
-		uint4 A = AS_UINT4(ctx_a); // ld.global.u32.v4
-		uint4 B = AS_UINT4(ctx_b);
+		uint4 A = __ldg((uint4 *)&d_ctx_a[thread << 2]);
+		uint4 B = __ldg((uint4 *)&d_ctx_b[thread << 2]);
 		uint32_t* a = (uint32_t*)&A;
 		uint32_t* b = (uint32_t*)&B;
+
+		__syncthreads();
 
 		for (int i = start; i < end; i++)
 		{
 			uint32_t c[4];
+			uint4 ls_tmp;
 			uint32_t j = (A.x >> 2) & E2I_MASK2;
 			cn_aes_single_round(sharedMemory, &long_state[j], c, a);
-			XOR_BLOCKS_DST(c, b, &long_state[j]);
-			store_variant1(&long_state[j]);
+			XOR_BLOCKS_DST(c, b, &ls_tmp);
+			store_variant1(&long_state[j], ls_tmp);
 			MUL_SUM_XOR_DST_1(c, a, &long_state[(c[0] >> 2) & E2I_MASK2], tweak);
 
 			j = (A.x >> 2) & E2I_MASK2;
 			cn_aes_single_round(sharedMemory, &long_state[j], b, a);
-			XOR_BLOCKS_DST(b, c, &long_state[j]);
-			store_variant1(&long_state[j]);
+			XOR_BLOCKS_DST(b, c, &ls_tmp);
+			store_variant1(&long_state[j], ls_tmp);
 			MUL_SUM_XOR_DST_1(b, a, &long_state[(b[0] >> 2) & E2I_MASK2], tweak);
 		}
 		if (bfactor) {
+			void * ctx_a = (void*)(&d_ctx_a[thread << 2]);
+			void * ctx_b = (void*)(&d_ctx_b[thread << 2]);
 			AS_UINT4(ctx_a) = A;
 			AS_UINT4(ctx_b) = B;
 		}
@@ -276,17 +283,18 @@ void cryptolight_gpu_phase2(const uint32_t threads, const uint16_t bfactor, cons
 }
 
 __global__
-void cryptolight_core_gpu_phase3(int threads, const uint32_t * long_state, uint32_t * ctx_state, uint32_t * ctx_key2)
+void cryptolight_core_gpu_phase3(const int threads, const uint32_t * long_state,
+	uint32_t * __restrict__ ctx_state, const uint32_t * ctx_key2)
 {
 	__shared__ uint32_t __align__(16) sharedMemory[1024];
-
-	cn_aes_gpu_init(sharedMemory);
 
 	const int thread = (blockDim.x * blockIdx.x + threadIdx.x) >> 3;
 	const int sub = (threadIdx.x & 7) << 2;
 
-	if(thread < threads)
+	if (thread < threads)
 	{
+		cn_aes_gpu_init(sharedMemory);
+
 		const int long_oft = (thread << LONG_SHL_IDX) + sub;
 		const int oft = thread * 50 + sub + 16;
 		uint32_t __align__(16) key[40];
@@ -300,7 +308,8 @@ void cryptolight_core_gpu_phase3(int threads, const uint32_t * long_state, uint3
 		AS_UINT2(&text[2]) = AS_UINT2(&ctx_state[oft + 2]);
 
 		__syncthreads();
-		for(int i = 0; i < LONG_LOOPS32; i += 32)
+
+		for (int i = 0; i < LONG_LOOPS32; i += 32)
 		{
 			#pragma unroll
 			for(int j = 0; j < 4; j++)
